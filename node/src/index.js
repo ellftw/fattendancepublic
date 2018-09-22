1f54aa6a86ab406c2d78e4eecb5ec299f8167c27
ellftw <alexkalaitzidis2@gmail.com>
2018-09-23 02:44:00 +0300

    registration table

--- node/src/index.js
+++ node/src/index.js
@@ -1,9 +1,28 @@
 const express = require("express")
 const http = require("http")
 
+let users = []
+
 let app = express()
 app.server = http.createServer(app)
 
+app.post('/service/register/:fingerprintID', (request, response) => {
+	if (users.indexOf(request.params.fingerprintID) === -1) {
+		users.push(request.params.fingerprintID)
+		console.log("New user registered")
+		console.log(users)
+		response.status(200).send()
+		}
+	else {
+		console.log("User already exists")
+		response.status(200).send()
+		}
+	})
+
+app.get('/service/attendance/:fingerprintID', (request, response) => {
+	console.log(request.params.fingerprintID)
+	response.status(200).send()
+	}) 
 
 app.get('/', (req, res) => {
 	res.json({ "message" : "server online"})
