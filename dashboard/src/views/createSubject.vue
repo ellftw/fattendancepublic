6a18edcec2dde15e9d742009d32e5d947e2bce73
ellftw <alexkalaitzidis2@gmail.com>
2019-06-14 10:58:27 +0300

    createsubject added

---
+++ dashboard/src/views/createSubject.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-container>
+    <v-layout xs10 offset-xs1>
+      <v-flex>
+        <form>
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            :counter="10"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+          <v-select
+            v-model="select"
+            :items="items"
+            :error-messages="selectErrors"
+            label="Item"
+            required
+            @change="$v.select.$touch()"
+            @blur="$v.select.$touch()"
+          ></v-select>
+          <v-checkbox
+            v-model="checkbox"
+            :error-messages="checkboxErrors"
+            label="Do you agree?"
+            required
+            @change="$v.checkbox.$touch()"
+            @blur="$v.checkbox.$touch()"
+          ></v-checkbox>
+          <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+            <v-btn flat color="primary" @click="$refs.menu.save(dates)">OK</v-btn>
+        </form>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs6>
+        <v-date-picker v-model="dates" multiple></v-date-picker>
+      </v-flex>
+      <v-flex xs6>
+        <v-menu
+          ref="menu"
+          v-model="menu"
+          :close-on-content-click="false"
+          :return-value.sync="dates"
+          min-width="290px"
+        >
+            <v-combobox
+              v-model="dates"
+              multiple
+              chips
+              small-chips
+              label="Multiple picker in menu"
+              prepend-icon="event"
+            ></v-combobox>
+          <v-date-picker v-model="dates" multiple no-title>
+          </v-date-picker>
+        </v-menu>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dates: ['2018-09-15", "2018-09-20'],
+    menu: false
+  })
+}
+</script>
+<style>
+</style>
